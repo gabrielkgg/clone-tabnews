a53748699a1e0f80a4db327fe48e46704534dde4
Gabriel Alves <30679485+gabrielkgg@users.noreply.github.com>
2025-03-15 13:54:31 +0000

    finalizado endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "uau, isto é magia" });
+  const updatedAt = new Date().toISOString();
+  const versaoBanco = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const nomeBanco = process.env.POSTGRES_DB;
+  const conexoesAtivas = await database.query({
+    text: "SELECT COUNT(*)::int AS count FROM pg_stat_activity WHERE datname = $1",
+    values: [nomeBanco],
+  });
+
+  console.log(conexoesAtivas.rows);
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versaoBanco.rows[0].server_version,
+        max_connections: +maxConnections.rows[0].max_connections,
+        active_connections: conexoesAtivas.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
